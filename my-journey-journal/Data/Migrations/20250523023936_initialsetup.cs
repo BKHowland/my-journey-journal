47af7986cabe45480b66b07785f45c1f1c972293
BKHowland <BenjaminKHowland@gmail.com>
2025-05-22 20:14:14 -0700

    Journal entries DB created using migrations and added to top bar

---
+++ my-journey-journal/Data/Migrations/20250523023936_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace my_journey_journal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JournalEntry",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EntryDetails = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JournalEntry", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JournalEntry");
+        }
+    }
+}
---
+++ my-journey-journal/Models/JournalEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace my_journey_journal.Models
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public string EntryName { get; set; }
+        public string EntryDetails { get; set; }
+
+
+        public JournalEntry()
+        {
+            
+        }
+    }
+}
