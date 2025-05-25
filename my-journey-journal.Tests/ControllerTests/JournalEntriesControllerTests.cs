fa7a042c0bae2f6cab8bf07f361295ed5710b3e2
BKHowland <BenjaminKHowland@gmail.com>
2025-05-24 20:25:17 -0700

    added mock in tests constructor for journalEntry controller dependency

---
+++ my-journey-journal.Tests/ControllerTests/JournalEntriesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using my_journey_journal.Data;
+
+namespace my_journey_journal.Tests.ControllerTests
+{
+    public class JournalEntriesControllerTests
+    {
+        private ApplicationDbContext _context;
+        public JournalEntriesControllerTests()
+        {
+            // Creating mock for all dependencies in the controller.
+            _context = A.Fake<ApplicationDbContext>();
+        }
+    }
+}
